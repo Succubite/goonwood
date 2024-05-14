9b63540b004028e4b61e2363fab9e3b92eb536ec
sovnya <enp7s1@tutanota.com>
2024-05-14 21:28:34 +0200

    More fucking setup...

--- Goonwood/Goonwood.cs
+++ Goonwood/Goonwood.cs
@@ -1,10 +1,7 @@
 using BepInEx;
 using BepInEx.Logging;
 // using Goonwood.Hooks;
-using System.Reflection;
-using System.Collections.Generic;
-using MonoMod.RuntimeDetour;
-using HarmonyLib;
+using Goonwood.Buttplug;
 
 namespace Goonwood;
 
@@ -13,18 +10,23 @@ public class Goonwood : BaseUnityPlugin
 {
     public static Goonwood Instance { get; private set; } = null!;
     internal new static ManualLogSource Logger { get; private set; } = null!;
+    internal static DeviceManager DeviceManager { get; private set; } = null!;
 
     private void Awake()
     {
         Logger = base.Logger;
         Instance = this;
 
+        // TODO: make `serverUri` configurable
+        DeviceManager = new DeviceManager("ViralTremors", "ws://127.0.0.1:12345");
+        DeviceManager.ConnectDevices();
+        
         Hook();
 
         Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} (v{MyPluginInfo.PLUGIN_VERSION}) has loaded!");
     }
 
-    internal static void Hook()
+    private static void Hook()
     {
         Logger.LogDebug("Hooking...");
 
