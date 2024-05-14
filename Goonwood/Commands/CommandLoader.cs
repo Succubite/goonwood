7c75fdb6733aab0836a570535bf96a5c582226e6
sovnya <enp7s1@tutanota.com>
2024-05-14 23:49:23 +0200

    Add basic template for adding custom commands to the game
    Although I haven't actually started on doing this, lol.
    
    I haven't even added BepInEx to my game install yet, not even MMHOOK, lol

---
+++ Goonwood/Commands/CommandLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Gloomwood.RuntimeConsole;
+
+namespace Goonwood.Commands;
+
+public class CommandLoader
+{
+    public static List<ConsoleCommand> LoadCommands()
+    {
+        return new List<ConsoleCommand>()
+        {
+            ReconnectCommand.GetCommand(),
+        };
+    }
+}
---
+++ Goonwood/Commands/ReconnectCommand.cs
@@ -0,0 +1,23 @@
+﻿using Gloomwood.RuntimeConsole;
+
+namespace Goonwood.Commands;
+
+public class ReconnectCommand
+{
+    public static ConsoleCommand GetCommand()
+    {
+        return new ConsoleCommand(Name, Description, Usage, Execute);
+    }
+
+    public static string Execute(params string[] args)
+    {
+        // TODO: this...
+        return "";
+    }
+
+    public static string Name = "BP-RECONNECT";
+
+    public static string Description = "Reconnect to the Buttplug server";
+
+    public static string Usage = "BP-RECONNECT";
+}
