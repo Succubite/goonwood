75b484c50123677dcafa403a85b27c8144e89871
sovnya <enp7s1@tutanota.com>
2024-11-23 15:55:57 +0100

    fix: Remove useless hook from PlayerHealthHooks

--- src/goonwood/Hooks/PlayerHealth.cs
+++ src/goonwood/Hooks/PlayerHealth.cs
@@ -10,16 +10,6 @@ public static class PlayerHealthHooks
     {
         PlayerHealth.OnPlayerDamage += PlayerHealthOnOnPlayerDamage;
         PlayerHealth.OnPlayerKilled += PlayerHealthOnOnPlayerKilled;
-        EntityHealth.SetAmount += EntityHealthOnSetAmount;
-    }
-
-    private static void EntityHealthOnSetAmount(EntityHealth.orig_SetAmount orig, Gloomwood.Entity.EntityHealth self,
-        int value)
-    {
-        orig(self, value);
-
-        if (self is not Gloomwood.Players.PlayerHealth) return;
-        Goonwood.Logger.LogInfo($"Player health set to {value}");
     }
 
     private static void PlayerHealthOnOnPlayerKilled(PlayerHealth.orig_OnPlayerKilled orig,
