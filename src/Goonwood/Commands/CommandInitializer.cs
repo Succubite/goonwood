5fe76f6af9989bac1d234e4f16465e27f3f540ec
bananasov <56086819+bananasov@users.noreply.github.com>
2026-06-05 17:31:43 +0200

    fix(commands): Hook RegisterCommands with postfix
    Was getting a null reference exception before due to `Console.commands`
    not being initialized. Why is that not set like `Dictionary<string,
    ConsoleCommand> commands = [];` lol

--- src/Goonwood/Commands/CommandInitializer.cs
+++ src/Goonwood/Commands/CommandInitializer.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using Gloomwood;
 using MonoDetour;
+using MonoDetour.Cil;
 using MonoDetour.HookGen;
+using MonoMod.Cil;
 
 namespace Goonwood.Commands;
 
@@ -13,7 +15,7 @@ public static class CommandInitializer
     [MonoDetourHookInitialize]
     public static void Initialize()
     {
-        Md.Gloomwood.Console.RegisterCommands.Prefix(Prefix_Start);
+        Md.Gloomwood.Console.RegisterCommands.Postfix(Postfix_RegisterCommands);
     }
 
     public static void AddCommand(ICommand command)
@@ -22,7 +24,7 @@ public static class CommandInitializer
         Commands.Add(command);
     }
 
-    private static void Prefix_Start()
+    private static void Postfix_RegisterCommands()
     {
         foreach (var command in Commands)
         {
